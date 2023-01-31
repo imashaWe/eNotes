dbabbe59515a6cf44d4f6a0fc0ecf6de3c2f26ea
imasshaWe <imasha98.we@gmail.com>
2023-01-31 19:13:25 +0530

    add crud operations

--- backend/src/routes/notes.routes.js
+++ backend/src/routes/notes.routes.js
@@ -10,22 +10,27 @@ router.post('/', (req, res) => {
         note
     });
     noteDoc.save()
-        .then(newNote => res.status(200).json({"message": "New note added"}))
-        .catch(e => res.status(200).json({"status": 0, "error": e.message}));
+        .then(newNote => res.json({'status': 1, "message": "New note added"}))
+        .catch(e => res.json({"status": 0, "error": e.message}));
 });
 
 router.get('/', (req, res) => {
     Note.find({})
         .then(notes => res.json(notes))
-        .catch(e => res.status(200).json({"status": 0, "error": e.modules}));
+        .catch(e => res.json({"status": 0, "error": e.modules}));
 });
 
 router.post('/:id', (req, res) => {
     const {title, note} = req.body;
     Note.updateOne({id: req.params.id}, {title, note})
         .then(updatedNote => res.status(200).json({"message": "Note updated"}))
-        .catch(e => res.status(200).json({"status": 0, "error": e.message}));
+        .catch(e => res.json({"status": 0, "error": e.message}));
 });
 
-router.post('')
+router.delete('/:id', (req, res) => {
+    Note.deleteOne({id: req.params.id})
+        .then(deletedNote => res.json({'status': 1, 'message': 'Note deleted'}))
+        .catch(e => res.json({'status': 0, 'message': e.message}));
+});
+
 module.exports = router;
